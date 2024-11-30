2d1cf9ffb2723d5bb7a5a848e31c0e67fe37e391
elmurodurinboev <elmurodorinboyev@gmail.com>
2024-11-30 12:05:22 +0500

    feat: add file system

--- app.js
+++ app.js
@@ -17,17 +17,24 @@ app.use(morgan("tiny"));
 app.use(authJwt()); // Apply the JWT middleware
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"))
 
-app.get(`${api}`, (req,res) => {
-  res.json({success: true})
-})
 
 
-// Connect to db
-mongoose
-  .connect(process.env.CONNECTION_STRING)
-  .then(() => console.log("DB is connected"))
-  .catch((err) => console.log(err));
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const bootstrap = async () => {
+  try {
+    await mongoose
+      .connect(process.env.CONNECTION_STRING)
+      .then(() => console.log("DB is connected"))
+      .catch((err) => console.log(err));
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error connecting with DB:", error)
+  }
+}
+
+
+bootstrap()
--- models/user.model.js
+++ models/user.model.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {type: String, required: true},
   phone: {type: String, required: true, unique: true},
-  email: {type: String, unique: true, sparse: true}, // Optional email field
+  email: {type: String, unique: true, sparse: true},
   passwordHash: {type: String, required: true},
   role: {type: String, enum: ['student', 'parent', 'teacher', 'admin'], required: true},
-  linkedStudentId: {type: mongoose.Schema.Types.ObjectId, ref: 'Student'}, // for parents
+  linkedStudentId: {type: mongoose.Schema.Types.ObjectId, ref: 'Student'},
 }, {timestamps: true});
 
 module.exports = mongoose.model('User', userSchema);
